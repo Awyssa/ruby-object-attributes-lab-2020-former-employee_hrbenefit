d383ec7cf286f25a687d0080c339a70e6ed2a6d3
Michael Ernest <m.hendersun@gmail.com>
2020-10-12 18:34:56 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,19 +1,19 @@
 class Dog
 
-  def name=(dogs_name)
-    @this_dogs_name = dogs_name
-  end
-
   def name
-    @this_dogs_name
+    @name
   end
 
-  def breed=(dogs_breed)
-    @this_dogs_breed = dogs_breed
+  def name=(name)
+    @name = name
   end
 
   def breed
-    @this_dogs_breed
+    @breed
+  end
+
+  def breed=(breed)
+    @breed = breed
   end
 
 end
